6a6823bf98a780b8450a9cbdae073696e9c2ec6d
Swolebrain <vic574m@gmail.com>
2015-08-07 21:23:56 -0400

    Finished
    simple slider using jquery effects

---
+++ slider-code.js
@@ -0,0 +1,28 @@
+$(function(){
+    $("#first").toggle();
+    var currentIndex = 0,
+      items = $('.caroussel-container div'),
+      itemAmt = items.length;
+    
+    function cycleItems() {
+        var item, fadeIn;
+        for (var i = 0; i < itemAmt; i++) {
+            var elem = items.eq(i);
+            if (elem.css("display") != "none") {
+                item = elem;
+                if (i == itemAmt-1) 
+                    fadeIn = items.eq(0);
+                else
+                    fadeIn = items.eq(i+1);
+                break;
+            }
+        }
+        item.hide("drop");
+        fadeIn.toggle("fade");
+    }
+    
+    var autoSlide = setInterval(function() {
+        cycleItems();
+    }, 3000);
+
+});
