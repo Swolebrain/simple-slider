9a0fbb0f6abca10cef55a5cf1f719479ce2d17a4
Swolebrain <vic574m@gmail.com>
2015-09-15 11:48:43 -0400

    somewhat working expanding slider

--- slider-code.js
+++ slider-code.js
@@ -1,10 +1,11 @@
 $(function(){
-        $("#first").toggle(); 
+    //$("#first").toggle(); 
+    $("#first").addClass("current-img-div");
     var currentIndex = 0,
         items = $('.caroussel-container div'),
         itemAmt = items.length;
     
-    function cycleItems() {
+    /*function cycleItems() {
         var item, fadeIn;
         for (var i = 0; i < itemAmt; i++) {
             var elem = items.eq(i); 
@@ -18,9 +19,27 @@ $(function(){
             }
         }
         item.hide("drop");
-        fadeIn.toggle("fade");
-        
-        
+        fadeIn.toggle("fade");  
+    }
+    
+    var autoSlide = setInterval(function() {
+        cycleItems();
+    }, 3000);*/
+    function cycleItems() {
+        var item, fadeIn;
+        for (var i = 0; i < itemAmt; i++) {
+            var elem = items.eq(i); 
+            if (elem.hasClass("current-img-div")) {
+                item = elem;
+                if (i == itemAmt-1) 
+                    fadeIn = items.eq(0);
+                else
+                    fadeIn = items.eq(i+1);
+                break;
+            }
+        }
+        fadeIn.addClass("current-img-div");
+        item.removeClass("current-img-div"); 
     }
     
     var autoSlide = setInterval(function() {
